ad2b52597c324eaf3dd5edcc9c827b2e995e97f2
Asif Khan <akhan@codeinreach.com>
2019-07-09 19:51:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,12 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {}
+	  :railroads => {
+	    :pieces => 4
+	    :names => {}
+	    :rent_in_dollars => {}
+	  }
+	   
 	}
 
 
@@ -16,18 +21,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {
-	        "mortgage_value" => "100"
-	      },
-	      :pennsylvania_railroad => {
-	        "mortgage_value" => "200"
-	      },
-	      :b_and_o_railroad => {
-	        "mortgage_value" => "400"
-	      },
-	      :shortline => {
-	        "mortgage_value" => "100"
-	      }
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline => {}
 	      
 	    },
 	    :rent_in_dollars{
